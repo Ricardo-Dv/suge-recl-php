cd15381b895efb7fa2c80edb0e53de2b655f0436
Ricardo <rjaimesv1505@gmail.com>
2020-05-02 04:28:26 -0400

    mi primer commit

---
+++ database/conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+$conn = mysqli_connect(
+  'localhost',
+  'root',
+  '',
+  'sugerenciasyreclamos'
+) or die(mysqli_erro($mysqli));
+
+
+?>
---
+++ vistas/escritorio.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Informes Sugerencias & Reclamos</title>
+    <!-- Bootstrap 4.4.1 con bootswatch 4.4.1 integrado -->
+    <link rel="stylesheet" href="../public/bootstrap/css/bootstrap.min.css">
+
+</head>
+<body>
+<!-- Navbar inicio -->
+<nav class="navbar navbar-dark bg-primary">
+<div class="container">
+    <a class="navbar-brand" href="escritorio.php">
+    <img src="../public/images/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    Sistema Sugerencias & Reclamos
+    </a>
+    
+</div> 
+
+</nav>
+    
+<!-- BOOTSTRAP 4 SCRIPTS -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="../public/boostrap/js/bootstrap.min.js"></script>
+    <!-- Font Awesome -->
+<script src="https://kit.fontawesome.com/c2b2462376.js" crossorigin="anonymous"></script>
+</body>
+</html>
