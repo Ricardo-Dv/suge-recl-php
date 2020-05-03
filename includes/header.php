42d1db4ba1d8af669a03eb44c8a2a439af0a8aa9
Ricardo <rjaimesv1505@gmail.com>
2020-05-03 05:48:52 -0400

    vista de login hecho

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Informes Sugerencias & Reclamos</title>
+    <!-- Bootstrap 4.4.1 con bootswatch 4.4.1 integrado -->
+    <link rel="stylesheet" href="../public/bootstrap/css/bootstrap.min.css">
+
+</head>
+<body>
+<!-- Navbar inicio -->
+<nav class="navbar navbar-dark bg-primary">
+<div class="container">
+    <a class="navbar-brand" href="escritorio.php">
+    <img src="../public/images/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    Sistema Sugerencias & Reclamos
+    </a>
+    
+</div> 
+
+</nav>
--- vistas/login.php
+++ vistas/login.php
@@ -1,11 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="../public/css/loginstyle.css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
 </head>
 <body>
-    
+	<img class="wave" src="../public/images/wavelogin.png">
+	<div class="container">
+		<div class="img">
+			<img src="../public/images/bglogin.svg">
+		</div>
+		<div class="login-content">
+			<form action="index.html">
+				<img src="../public/images/avatarlogin.svg">
+				<h2 class="title">Bienvenido</h2>
+           		<div class="input-div one">
+           		   <div class="i">
+           		   		<i class="fas fa-user"></i>
+           		   </div>
+           		   <div class="div">
+           		   		<h5>Usuario</h5>
+           		   		<input type="text" class="input">
+           		   </div>
+           		</div>
+           		<div class="input-div pass">
+           		   <div class="i"> 
+           		    	<i class="fas fa-lock"></i>
+           		   </div>
+           		   <div class="div">
+           		    	<h5>Contraseña</h5>
+           		    	<input type="password" class="input">
+            	   </div>
+            	</div>
+            	<a href="#">Contraseña olvidada?</a>
+            	<input type="submit" class="btn" value="Login">
+            </form>
+        </div>
+    </div>
+    <script type="text/javascript" src="../public/js/loginjs.js"></script>
 </body>
 </html>
